7ba1b24b5a0527cde04bd9814b452a1cbece95b0
Navegador <Navegador@RCAL1306P10-111>
2025-08-18 08:31:29 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Pedro";
+            string apellido;
+            int edad;
+            Console.WriteLine("Proyecto de consola con .NET Framework....");
+            Console.WriteLine("Buenos días:{0}", nombre);
+            Console.WriteLine("Ingrese Apellido: {0}", nombre);
+            apellido = Console.ReadLine();
+            Console.WriteLine("Buenos días:{0} {1}", nombre, apellido);
+            Console.WriteLine("Ingrese Edad:");
+            edad = int.Parse(Console.ReadLine());
+            // Forma ALternativa: edad = Convert.ToInt32(edad);
+            Console.WriteLine("Buenos días:{0} {1} usted tiene {2} años.", nombre, apellido, edad);
+            Console.ReadLine();
+        }
+    }
+}
