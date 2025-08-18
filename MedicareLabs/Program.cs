69ec0d62d5b0ef58dcad9184598d77cdace2fa40
Carlos Gabriel Ortmann <o.carlosguiller@gmail.com>
2025-08-18 10:25:02 -0300

    ejemplos de la clase por el profesor

---
+++ MedicareLabs/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace MedicareLabs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int PAC = 15;
+            const int PRU = 8;
+            int[,] Resultados = new int[PAC, PRU];
+
+            Console.WriteLine("MedicareLabs");
+        }
+    }
+}
