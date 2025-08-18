98c2166b52ab3473cd3a7e0595efbf54bc8bec02
Carlos Gabriel Ortmann <o.carlosguiller@gmail.com>
2025-08-18 10:19:15 -0300

    ejemplos de la clase del profe

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -5,6 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            int valor = 20;
+            var numero = "Pedro";
+            Console.WriteLine("El nombre es: " + numero + " Y la edad es: " + valor);
+
+            Console.WriteLine("El nombre es: {0} y la edad es: {1}", numero, valor);
+
+            Console.WriteLine($"El nombre es: {numero} y la edad es: {valor}");
+
         }
     }
 }
