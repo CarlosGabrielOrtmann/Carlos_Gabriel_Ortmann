2c346d3601823e9802808643ed09306da5031f58
Carlos Gabriel Ortmann <o.carlosguiller@gmail.com>
2025-08-18 11:53:37 -0300

    Agregado la simplificacion del ContarPacienteValorAlto

--- MedicareLabs/Program.cs
+++ MedicareLabs/Program.cs
@@ -4,21 +4,22 @@ namespace MedicareLabs
 {
     internal class Program
     {
-        const int PAC = 15;
-        const int PRU = 8;
+        const int PAC = 3;
+        const int PRU = 2;
 
         static void Main(string[] args)
         {
             
             int[,] Resultados = new int[PAC, PRU];
-            int[] MinimoPrueba = new int[PRU] { 5, 10, 10, 20, 30, 40, 30, 50 };
-            int[] MaximoPrueba = new int[PRU] { 20, 30, 40, 70, 80, 90, 120, 150 };
+            //int[] MinimoPrueba = new int[PRU] { 5, 10, 10, 20, 30, 40, 30, 50 };
+            int[] MaximoPrueba = new int[PRU] { 20, 30 }; //, 40, 70, 80, 90, 120, 150 };
            
 
             Console.WriteLine("MedicareLabs");
 
             IngresarResultadosPruebas(Resultados);
-            ContarPAcientesValorAbsoluto(Resultados, MaximoPrueba);
+            ContarPacientesValorAlto(Resultados, MaximoPrueba);
+            ContarPacientesValorAlto_2(Resultados, MaximoPrueba);
             CalcularPromedioPruebaEspecifica(Resultados);
             BuscarValorMinimoMAximoPacientes(Resultados);
             PorcentajeResultadosAnormales(Resultados);
@@ -36,13 +37,15 @@ namespace MedicareLabs
                 {
                     do
                     {
-                        Console.Write($"Ingrese el resultado de la resultadosAnalisis {j+1}");
+                        Console.Write($" Ingrese el resultado de la prueba {j+1}: ");
                         resultadosAnalisis = Convert.ToInt32( Console.ReadLine() );
                     } while (resultadosAnalisis < 0);
                     Resultados[i,j] = resultadosAnalisis;
                 }
             }
+
             Console.ReadKey();
+
             for (int i=0; i < PAC; i++)
             {
                 Console.Write($"Paciente: {i + 1}");
@@ -55,7 +58,7 @@ namespace MedicareLabs
         
        
 
-        public static void ContarPAcientesValorAbsoluto(int[,] Resultados, int[] MaximoPrueba)
+        public static void ContarPacientesValorAlto(int[,] Resultados, int[] MaximoPrueba)
         {
             //contar
             int[] PacienteAltos = new int[PAC];
@@ -94,6 +97,71 @@ namespace MedicareLabs
                 }
             }
             //mostrar
+            for (int i = 0; i < PAC; i++)
+            {
+                Console.Write($"Paciente: {PacienteNumero[i]} Cantidad: {PacienteAltos[i]}");
+            }
+        }
+
+        public static void ContarPacientesValorAlto_2(int[,] Resultados, int[] MaximoPrueba)
+        {
+            int[] PacienteAltos = new int[PAC];
+            int[] PacienteNumero = new int[PAC];
+
+            ContarAltos(Resultados, MaximoPrueba, PacienteAltos, PacienteNumero);
+
+            OrdenarAltos(PacienteAltos, PacienteNumero);
+
+            MostrarAltos(PacienteAltos, PacienteNumero);
+
+            int cuentaAltos, auxiliar;
+        }
+
+        public static void ContarAltos(int[,] Resultados, int[] MaximoPrueba, int[] PacienteAltos, int[] PacienteNumero)
+        {
+            int cuentaAltos, auxiliar;
+
+            for (int i = 0; i < PAC; i++)
+            {
+                cuentaAltos = 0;
+                Console.Write($"Paciente: {i + 1}");
+                for (int j = 0; j < PRU; j++)
+                {
+                    if (Resultados[i, j] > MaximoPrueba[j])
+                    {
+                        cuentaAltos++;
+                    }
+                }
+                PacienteAltos[i] = cuentaAltos;
+                PacienteNumero[i] = i + 1;
+            }
+        }
+
+
+        public static void OrdenarAltos(int[] PacienteAltos, int[] PacienteNumero)
+        {
+            int auxiliar;
+
+            for (int i = 0; i < PAC; i++)
+            {
+                for (int j = 0; j < PRU; j++)
+                {
+                    if (PacienteAltos[i] < PacienteAltos[j])
+                    {
+                        auxiliar = PacienteAltos[i];
+                        PacienteAltos[i] = PacienteAltos[j];
+                        PacienteAltos[j] = auxiliar;
+
+                        auxiliar = PacienteNumero[i];
+                        PacienteNumero[i] = PacienteNumero[j];
+                        PacienteNumero[j] = auxiliar;
+                    }
+                }
+            }
+        }
+
+        public static void MostrarAltos(int[] PacienteAltos,int[] PacienteNumero)
+        {
             for (int i = 0; i < PAC; i++)
             {
                 Console.Write($"Paciente: {PacienteNumero[i]} Cantidad: {PacienteAltos[i]}");
